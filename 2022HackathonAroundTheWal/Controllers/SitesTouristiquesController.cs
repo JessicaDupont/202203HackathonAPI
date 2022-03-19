fabe715d94c0653009b14692e6fa85c4116e2859
Jessica DUPONT <jessica.dupont1990@gmail.com>
2022-03-19 14:47:30 +0100

    base

---
+++ 2022HackathonAroundTheWal/Controllers/SitesTouristiquesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _2022HackathonAroundTheWal.Controllers
+{
+    public class SitesTouristiquesController : Controller
+    {
+        // GET: SitesTouristiquesController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: SitesTouristiquesController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //GET: SitesTouristiquesController/Details/Balade
+        public ActionResult Search(string critere)
+        {
+            return View();
+        }
+    }
+}
