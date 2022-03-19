71edf15f54528672b47ae6df83e7ee24b9f4d98b
Jessica DUPONT <jessica.dupont1990@gmail.com>
2022-03-19 18:39:53 +0100

    Interfaces Lieux (bd + front)

--- 2022HackatonAroundTheWal/Bases/ILieu.cs
+++ 2022HackatonAroundTheWal/Bases/ILieu.cs
@@ -6,5 +6,15 @@ namespace _2022HackatonAroundTheWal.Bases
 {
     public interface ILieu
     {
+        string Nom { get; set; }
+        string ImageUrl { get; set; }
+        string Description { get; set; }
+        public double GeoLat { get; set; }
+        public double GeoLong { get; set; }
+        public string Adresse { get; set; }
+        public int CodePostal { get; set; }
+        public string Localite { get; set; }
+        public string SiteUrl { get; set; }
+        public IEnumerable<ICategorie> Categories { get; set; }
     }
 }
